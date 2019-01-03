ee4eae477be626f16441c358bd959cbe6c351a4c
tactshan <329835044@qq.com>
2019-01-03 14:13:28 +0800

    shop

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Model\UserModel;
+
+class UserController extends Controller
+{
+    //
+
+	public function user($uid)
+	{
+		echo $uid;
+	}
+
+	public function test()
+    {
+        echo '<pre>';print_r($_GET);echo '</pre>';
+    }
+
+	public function add()
+	{
+		$data = [
+			'name'      => str_random(5),
+			'age'       => mt_rand(20,99),
+			'email'     => str_random(6) . '@gmail.com',
+			'reg_time'  => time()
+		];
+
+		$id = UserModel::insertGetId($data);
+		var_dump($id);
+	}
+	public function data()
+    {
+	    echo date('Y-m-d H:i:s');
+    }
+    public function usershow()
+    {
+	        $info=UserModel::all()->toArray();
+	        return view('user.userlist',$info);
+    }
+}
