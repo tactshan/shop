a847fee5f0fb696f8c22cd0e356d18a7cd1880bf
tactshan <329835044@qq.com>
2019-01-15 19:34:48 +0800

    shop

--- app/Http/Controllers/Pay/CrontabController.php
+++ app/Http/Controllers/Pay/CrontabController.php
@@ -21,29 +21,23 @@ class CrontabController extends Controller
         if(empty($orderInfo)){
             echo 'NULL';exit;
         }
-        var_dump($orderInfo);exit;
-//        $res=false;
-//        foreach ($orderInfo as $k=>$v){
-//            if($v['order_status']==1){
-//                echo '1';
-//                if(time()-$v['c_time'] > 300){
-//                    echo '2';
-//                    $Orderwhere=['order_num'=>$v['order_num']];
-//                    $data=[
-//                        'order_status'=>3
-//                    ];
-//                    $res=OrderModel::where($Orderwhere)->update($data);
-//                    var_dump($res);
-//                }
-//            }
-//        }
-//        echo '3';
-//        var_dump($res);
-//        if($res!==false){
-//            echo "Success"."\n";
-//        }else{
-//            echo 'Error';exit;
-//        }
+        $res=false;
+        foreach ($orderInfo as $k=>$v){
+            if($v->order_status==1){
+                if(time()-$v['c_time'] > 300){
+                    $Orderwhere=['order_num'=>$v->order_num];
+                    $data=[
+                        'order_status'=>3
+                    ];
+                    $res=OrderModel::where($Orderwhere)->update($data);
+                }
+            }
+        }
+        if($res!==false){
+            echo "Success"."\n";
+        }else{
+            echo 'Error';exit;
+        }
 
     }
 
