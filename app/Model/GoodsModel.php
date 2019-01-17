a9868f452d4396fcae4b66ddfb0c5deb39ce490f
tactshan <329835044@qq.com>
2019-01-17 20:16:41 +0800

    admin

--- app/Model/GoodsModel.php
+++ app/Model/GoodsModel.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoodsModel extends Model
 {
-    public $table = 'goods';
+    public $table = 'shop_goods';
     public $timestamps = false;
+    public $primaryKey = 'goods_id';
+    //获取某字段时 格式化 该字段的值
+    public function getGoodsPriceAttribute($price)
+    {
+        return $price / 100;
+    }
 }
