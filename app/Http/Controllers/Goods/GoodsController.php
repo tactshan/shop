50da3ed29552ea938ad4f11e6dbf083d6637ccba
tactshan <329835044@qq.com>
2019-01-23 10:56:11 +0800

    shop

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -3,21 +3,16 @@
 namespace App\Http\Controllers\Goods;
 
 use App\Model\GoodsModel;
+use Illuminate\Filesystem\Cache;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Redis as Redis;
+
 
 class GoodsController extends Controller
 {
     //商品列表展示
     public function goodsList(){
-
-        $redis = new redis();
-        $result = $redis->connect('127.0.0.1', 6379);
-        var_dump($result);exit;
-
-
 
         if(!empty($_GET['key'])){
             $key=$_GET['key'];
@@ -27,15 +22,15 @@ class GoodsController extends Controller
 
         $cacheKey='info';
 
-        if(Redis::exists($cacheKey)){
-            $res = Redis::get($cacheKey);
+        if(Cache::exists($cacheKey)){
+            $res = Cache::get($cacheKey);
             $info = unserialize($res);
         }else{
             $info=DB::table('shop_goods')->where('goods_name','like',"%$key%")->paginate(2);
         }
 
         //存redis
-        Redis::setex($cacheKey, 600, serialize($info));
+        Cache::setex($cacheKey, 10, serialize($info));
 
         $uid=session()->get('uid');
         $data=[
