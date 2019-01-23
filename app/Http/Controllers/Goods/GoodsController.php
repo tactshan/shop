ec0857acaf53802fffa503f962b73aa6e6462dc6
tactshan <329835044@qq.com>
2019-01-23 11:10:21 +0800

    shop

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -7,6 +7,7 @@ use Illuminate\Filesystem\Cache;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
 
 
 class GoodsController extends Controller
