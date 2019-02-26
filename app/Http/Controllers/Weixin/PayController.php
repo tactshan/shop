ff311b0e9a0d18bc7d932f6127aaf308aa8235ce
tactshan <329835044@qq.com>
2019-02-26 10:29:16 +0000

    debug

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Weixin;
 
+use App\Model\OrderModel;
+use App\Model\UserModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -18,7 +20,7 @@ class PayController extends Controller
             'mch_id'        =>  env('WEIXIN_MCH_ID'),       // 商户ID
             'nonce_str'     => str_random(16),             // 随机字符串
             'sign_type'     => 'MD5',
-            'body'          => 'Tactshan的订单测试-'.mt_rand(1111,9999) . str_random(6),
+            'body'          => 'Tactshan的订单测试',
             'out_trade_no'  => $order_num,                       //本地订单号
             'total_fee'     => $total_fee,
             'spbill_create_ip'  => $_SERVER['REMOTE_ADDR'],     //客户端IP
@@ -124,30 +126,89 @@ class PayController extends Controller
     public function notice()
     {
         $data = file_get_contents("php://input");
-
         //记录日志
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
         file_put_contents('logs/wx_pay_notice.log',$log_str,FILE_APPEND);
 
-        $xml = simplexml_load_string($data);
-
-        if($xml->result_code=='SUCCESS' && $xml->return_code=='SUCCESS'){      //微信支付成功回调
+        $xml = (array)simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if($xml['result_code']=='SUCCESS' && $xml['return_code']=='SUCCESS'){      //微信支付成功回调
             //验证签名
-            $sign = true;
-
+            $sign = $this->check_sign($xml);
             if($sign){       //签名验证成功
-                //TODO 逻辑处理  订单状态更新
-
+                //记录日志
+                $log_str = '>>>> ' . date('Y-m-d H:i:s');
+                if($sign === false){
+                    //记录日志 验签失败
+                    $log_str .= " Sign Failed!<<<<< \n\n";
+                    file_put_contents('logs/wx_pay.log',$log_str,FILE_APPEND);
+                }else{
+                    $log_str .= " Sign OK!<<<<< \n\n";
+                    file_put_contents('logs/wx_pay.log',$log_str,FILE_APPEND);
+                }
+                //逻辑处理  订单状态更新
+                $res=$this->dealOrder($xml);
+                if($res){
+                    $order_num=$xml['out_trade_no'];
+                    header("refresh:1;url='/orderdetail/$order_num");
+                }else{
+                    die('Error');
+                }
             }else{
                 //TODO 验签失败
                 echo '验签失败，IP: '.$_SERVER['REMOTE_ADDR'];
                 // TODO 记录日志
             }
-
         }
-
         $response = '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
         echo $response;
+    }
+    //验签
+    public function check_sign($xml)
+    {
+        $this->values = [];
+        $this->values = $xml;
+        $sign=$this->SetSing();
+        if($sign!==$xml['sign']){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    //处理订单逻辑
+    function dealOrder($data){
+        $order_num=$data['out_trade_no'];
+        $orderWhere=[
+            'order_num'=>$order_num
+        ];
+        $orderData=OrderModel::where($orderWhere)->first()->toArray();
+        if(empty($orderData)){
+            die("订单 ".$order_num. "不存在！");
+        }
+        $order_status=$orderData['order_status'];
+        if($order_status!=1){
+            die("此订单已被支付或订单异常。");
+        }
 
+        $order_amount=$orderData['order_amount'];
+        //更改订单状态
+        $where=[
+            'order_num'=>$order_num
+        ];
+        $data=[
+            'order_status'=>2
+        ];
+        $res=OrderModel::where($where)->update($data);
+        //根据订单号获查询订单表获取到用户id
+        $uid=$orderData['user_id'];
+        //赠送积分
+        $userWhere=[
+            'uid'=>$uid
+        ];
+        $userData=UserModel::where($userWhere)->first()->toArray();
+        $userData['integral']=$userData['integral']+$order_amount;
+        $res2=UserModel::where($userWhere)->update($userData);
+        if($res&&$res2){
+            return true;
+        }
     }
 }
