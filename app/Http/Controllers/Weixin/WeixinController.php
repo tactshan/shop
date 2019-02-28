c5bf3abc84bae6cc7c9361c0278e8fcebf333d6b
tactshan <329835044@qq.com>
2019-02-28 11:30:32 +0000

    debug

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -534,7 +534,8 @@ class WeixinController extends Controller
 //                header("refresh:2;url=/goodslist");exit;
 //            }
         }else{
-            $request->session()->put('uid',$userInfo->uid);
+            $uid=$userInfo->uid;
+            $request->session()->put('uid',$uid);
             setcookie('cookie_token',$token,time()+86400,'','',false,true);
             $request->session()->put('u_token',$token);
             header("refresh:2;url=/goodslist");exit;
