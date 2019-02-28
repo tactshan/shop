14b34d195fa037566fd7189ed44e96c3326c3647
tactshan <329835044@qq.com>
2019-02-28 05:38:20 +0000

    微信等陆测试

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -485,11 +485,24 @@ class WeixinController extends Controller
      */
     public function getCode()
     {
+        //1获取code(微信登录成功，即获得code)
         $code=$_GET['code'];
+        //2根据code获取access_token 和 openid
         $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe24f70961302b5a5&secret=0f121743ff20a3a454e4a12aeecef4be&code='.$code.'&grant_type=authorization_code';
         $token_json = file_get_contents($token_url);
         $token_arr = json_decode($token_json,true);
         echo '<hr>';
         echo '<pre>';print_r($token_arr);echo '</pre>';
+
+        $access_token = $token_arr['access_token'];
+        $openid = $token_arr['openid'];
+
+        // 3 携带token  获取用户信息
+        $user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $user_json = file_get_contents($user_info_url);
+
+        $user_arr = json_decode($user_json,true);
+        echo '<hr>';
+        echo '<pre>';print_r($user_arr);echo '</pre>';
     }
 }
