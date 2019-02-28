ae447354295553562e8858ea8e388c7f5cafcc2b
tactshan <329835044@qq.com>
2019-02-28 11:32:37 +0000

    debug

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -22,12 +22,12 @@ class CheckLogin
 //        $cookie_token=$request->cookie('cookie_token');
 //        echo $cookie_token."<br>";
         if(empty($uid)){
-            echo "Place Login....";
+            echo "Place Login....001";
             header("refresh:2;url=/userlogin");
             exit;
         }
         if($_COOKIE['cookie_token'] != $access_token) {
-            echo "Place Login....";
+            echo "Place Login....002";
             header("refresh:2;url=/userlogin");
             exit;
         }
