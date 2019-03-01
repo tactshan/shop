77eec55219b495ea7a972fca7848ea4311bf975a
tactshan <329835044@qq.com>
2019-03-01 06:02:21 +0000

    debug

--- app/Http/Controllers/Weixin/WeixinJsSDK.php
+++ app/Http/Controllers/Weixin/WeixinJsSDK.php
@@ -33,7 +33,7 @@ class WeixinJsSDK extends Controller
      */
     public function wxJsConfigSign($param)
     {
-        $current_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];     //当前调用 jsapi的 url
+        $current_url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];     //当前调用 jsapi的 url
         $ticket=$this->getJsapiTicket();
         $str =  'jsapi_ticket='.$ticket.'&noncestr='.$param['noncestr']. '&timestamp='. $param['timestamp']. '&url='.$current_url;
         $signature=sha1($str);
