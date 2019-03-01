431bcd52e7eaeb693e58981b68a1c0530de2d099
tactshan <329835044@qq.com>
2019-03-01 07:48:55 +0000

    获取地理位置

--- resources/views/weixin/wx_jssdk.blade.php
+++ resources/views/weixin/wx_jssdk.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1><font>JS-SDK</font></h1>
     <button id="img">选择图片</button>
+    <button id="saosao">扫一扫</button>
 @endsection
 @section('footer')
     @parent
@@ -23,6 +24,17 @@
                 sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                 success: function (res) {
                     var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                }
+            });
+        })
+        $('#saosao').click(function () {
+            wx.getLocation({
+                type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                success: function (res) {
+                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                    var speed = res.speed; // 速度，以米/每秒计
+                    var accuracy = res.accuracy; // 位置精度
                 }
             });
         })
